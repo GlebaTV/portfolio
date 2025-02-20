5888ca48883924b31c04cb78bb9d5acfc274c282
GlebaTV <gleb17102000@gmail.com>
2025-02-20 07:02:27 -0600

    Fixed image paths1

--- src/components/ProjectCard/ProjectsList.jsx
+++ src/components/ProjectCard/ProjectsList.jsx
@@ -5,19 +5,19 @@ const projects = [
   {
     title: 'The Burger House website',
     description: 'Burger shop website with menu, promotions, ordering and easy navigation..',
-    imageUrl: '/assets/images/prj1.jpg',
+    imageUrl: '/portfolio/assets/images/prj1.jpg',
     link: 'https://glebatv.github.io/Burger-site/'
   },
   {
     title: 'University',
     description: 'University site with information about the faculties, timetable, news and contacts.',
-    imageUrl: '/assets/images/prj2.jpg',
+    imageUrl: '/portfolio/assets/images/prj2.jpg',
     link: 'https://glebatv.github.io/University/'
   },
   {
     title: 'Music Player',
     description: 'Music player site with playlists, track management and settings.',
-    imageUrl: '/assets/images/prj3.jpg',
+    imageUrl: '/portfolio/assets/images/prj3.jpg',
     link: 'https://glebatv.github.io/Music-Player/'
   },
 ];
